af8f2ccdceae0f85ee388e0c04af43d37762e4c6
hidden4003 <hidden@4you.lt>
2018-03-12 11:04:49 +0200

    Fix lockup on login screen when server offline or can't provide a login image

--- app/sagas/api.js
+++ app/sagas/api.js
@@ -3,10 +3,10 @@ function apiCallPost(apiAction, apiParams, apiKey, apiHost) {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      apikey: apiKey,
+      apikey: apiKey
     },
     body: JSON.stringify(apiParams),
-    method: 'POST',
+    method: 'POST'
   });
 }
 
@@ -15,38 +15,51 @@ function apiCallGet(apiAction, apiParams, apiKey, apiHost) {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      apikey: apiKey,
-    },
+      apikey: apiKey
+    }
   });
 }
 
 function apiCall(apiState, apiAction, apiParams, type = 'GET') {
-  const fetch = type === 'POST' ? apiCallPost(apiAction, apiParams, apiState.key, apiState.host) : apiCallGet(apiAction, apiParams, apiState.key, apiState.host);
+  const fetch =
+    type === 'POST'
+      ? apiCallPost(apiAction, apiParams, apiState.key, apiState.host)
+      : apiCallGet(apiAction, apiParams, apiState.key, apiState.host);
 
-  return fetch.then((response) => {
+  return fetch.then(response => {
     if (response.status !== 200) {
-      return Promise.reject(`Network error: ${apiAction} ${response.status}: ${response.statusText}`);
+      return Promise.reject(
+        `Network error: ${apiAction} ${response.status}: ${response.statusText}`
+      );
     }
     return Promise.resolve(response);
   });
 }
 
 function jsonApiCall(apiState, apiAction, apiParams, type) {
-  return apiCall(apiState, apiAction, apiParams, type)
-    .then(response => response.json());
+  return apiCall(apiState, apiAction, apiParams, type).then(response =>
+    response.json()
+  );
 }
 
 function jsonApiResponse(apiState, apiAction, apiParams, type) {
   return jsonApiCall(apiState, apiAction, apiParams, type)
-    .then((json) => {
+    .then(json => {
       if (json.code) {
         if (json.code !== 200) {
-          return { error: true, code: json.code, message: json.message || 'No error message given.' };
+          return {
+            error: true,
+            code: json.code,
+            message: json.message || 'No error message given.'
+          };
         }
       }
       return { data: json };
     })
-    .catch(reason => ({ error: true, message: typeof reason === 'string' ? reason : reason.message }));
+    .catch(reason => ({
+      error: true,
+      message: typeof reason === 'string' ? reason : reason.message
+    }));
 }
 
 function getGroups(apiState) {
@@ -55,14 +68,22 @@ function getGroups(apiState) {
 
 function getGroupFilters(apiState, id) {
   if (id) {
-    return jsonApiResponse(apiState, `/filter?level=1&nocast=1&notag=1&id=${id}`, '');
+    return jsonApiResponse(
+      apiState,
+      `/filter?level=1&nocast=1&notag=1&id=${id}`,
+      ''
+    );
   }
   return jsonApiResponse(apiState, '/filter', '');
 }
 
 function getSeries(apiState, id) {
   if (id) {
-    return jsonApiResponse(apiState, `/serie?allpics=1&tagfilter=63&level=1&id=${id}`, '');
+    return jsonApiResponse(
+      apiState,
+      `/serie?allpics=1&tagfilter=63&level=1&id=${id}`,
+      ''
+    );
   }
   return jsonApiResponse(apiState, '/serie?nocast=1&tagfilter=63', '');
 }
@@ -72,14 +93,16 @@ function postLogin(host, data) {
 }
 
 function getRandomImage(apiState) {
-  return apiCall(apiState, '/image/7/random', '');
+  return apiCall(apiState, '/image/7/random', '').catch(reason => ({
+    error: true,
+    message: typeof reason === 'string' ? reason : reason.message
+  }));
 }
-
 
 export default {
   getGroups,
   getSeries,
   postLogin,
   getGroupFilters,
-  getRandomImage,
+  getRandomImage
 };
